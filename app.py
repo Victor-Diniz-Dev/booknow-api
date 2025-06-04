605a78d2e602af5efc5a894a56ad4b87f983ba9d
Victor-Diniz-Dev <victordinizdev@gmail.com>
2025-06-04 01:38:12 -0300

    add endpoint de cadastro e login de user

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
 from sqlalchemy import create_engine, text
+from flask import request
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-
 
 DATABASE_URL = "postgresql://booknow_db_user:qGqC8626TL2nWm4myDWrQOzOE2ioJxnh@dpg-d0vq8f3ipnbc7386864g-a.oregon-postgres.render.com/booknow_db"
 engine = create_engine(DATABASE_URL)
@@ -55,7 +56,58 @@ def livros_por_genero(genero_id):
         )
         livros = [dict(row._mapping) for row in result]
         return jsonify(livros)
+    
 
+#endpoint cadastro user    
+@app.route("/usuarios", methods=["POST"])
+def cadastrar_usuario():
+    data = request.get_json()
+
+    nome = data.get("nome")
+    email = data.get("email")
+    senha = data.get("senha")
+
+    if not nome or not email or not senha:
+        return jsonify({"erro": "Campos obrigatórios: nome, email e senha"}), 400
+
+    senha_hash = generate_password_hash(senha)
+
+    try:
+        with engine.connect() as connection:
+            connection.execute(
+                text("""
+                    INSERT INTO usuarios (nome, email, senha_hash)
+                    VALUES (:nome, :email, :senha)
+                """),
+                {"nome": nome, "email": email, "senha": senha_hash}
+            )
+        return jsonify({"mensagem": "Usuário cadastrado com sucesso!"}), 201
+
+    except Exception as e:
+        return jsonify({"erro": "Erro ao cadastrar usuário", "detalhe": str(e)}), 500
+    
+#verifica se email+senha bate com o banco
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    email = data.get("email")
+    senha = data.get("senha")
+
+    if not email or not senha:
+        return jsonify({"erro": "Informe email e senha"}), 400
+
+    with engine.connect() as connection:
+        result = connection.execute(
+            text("SELECT * FROM usuarios WHERE email = :email"),
+            {"email": email}
+        )
+        usuario = result.fetchone()
+
+        if usuario and check_password_hash(usuario._mapping["senha_hash"], senha):
+            return jsonify({"mensagem": "Login bem-sucedido", "nome": usuario._mapping["nome"]})
+        else:
+            return jsonify({"erro": "Credenciais inválidas"}), 401
 
 
 #uptime robot - serviço que nao deixa a api dormir
